2b446c369822cdff5b6eb5ff8c5569fca971abe6
Antanina Druzhkina <totora246@gmail.com>
2024-03-10 21:00:32 +0400

    add project

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace WebApplicationTestProject1.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name cant be empty")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "ImageUrl cant be empty")]
+
+        public string ImageUrl { get; set; }
+    }
+}
